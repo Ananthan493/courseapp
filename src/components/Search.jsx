58b5928d0a3ce3bda71531b5ce460c079399e8e3
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-12 11:16:38 +0530

    search completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const Search = () => {
+    const[data,setData]=useState(
+        {
+            "title":""
+        }
+    )
+    const[result,changeResult]=useState([])
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]: event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8084/search",data).then(
+            (response)=>{
+                changeResult(response.data)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        ).finally()
+       
+    }
   return (
     <div>
         <Navbar/>
@@ -9,10 +32,44 @@ const Search = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="label form-label">Course Title</label>
-                    <input type="text" className="input form-control" />
+                    <input type="text" className="input form-control" name='title' value={data.title} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-secondary">Search</button>
+                    <button className="btn btn-secondary"onClick={readValue}>Search</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Course Title</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Duration</th>
+                                    <th scope="col">Venue</th>
+                                    <th scope="col">Trainer Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    result.map(
+
+                                        (value, index) => {
+                                            return <tr>
+                                                <td>{value.title}</td>
+                                                <td>{value.description}</td>
+                                                <td>{value.date}</td>
+                                                <td>{value.duration}</td>
+                                                <td>{value.venue}</td>
+                                                <td>{value.name}</td>
+                                            </tr>
+
+                                        }
+                                    )   
+                                }
+                            </tbody>
+                        </table>
                 </div>
             </div>
         </div>
